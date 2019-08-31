9bf9f2f885f87572f05cd8e33eb179da0170e60d
msoft <msoft2000@gmail.com>
2019-08-31 04:33:23 +0200

    Adding proxy for calling native DLLs.

--- ArchitectureExample/Program.cs
+++ ArchitectureExample/Program.cs
@@ -1,22 +1,24 @@
-﻿using MixedAssembly;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using DependencyLoader;
 
 namespace ArchitectureExample
 {
 	class Program
-	{
+	{		
 		static void Main(string[] args)
 		{
 			string textToDisplay = "text to display";
 
 			Console.WriteLine($"Displaying from managed code: {textToDisplay}");
 
-			NativeCaller nativeCaller = new NativeCaller(textToDisplay);
-			nativeCaller.CallNativeCode();
+			var proxy = new NativeCallerProxy();
+			proxy.CallNativeCaller(textToDisplay);
 
 			Console.ReadLine();
 		}
---
+++ DependencyLoader/NativeCallerProxy.cs
@@ -0,0 +1,41 @@
+﻿using MixedAssembly;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyLoader
+{
+    public class NativeCallerProxy
+    {
+		private const string dependencySubFolder = "Dependencies";
+
+		public NativeCallerProxy()
+		{
+			CopyDependencies();
+		}
+
+		public void CallNativeCaller(string textToDisplay)
+		{
+			NativeCaller nativeCaller = new NativeCaller(textToDisplay);
+			nativeCaller.CallNativeCode();
+		}
+
+		private static void CopyDependencies()
+		{
+			string executableFolder = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+			string dependencyArchitecture = Environment.Is64BitProcess ? "x64" : "Win32";
+			string dependencyFolder = Path.Combine(executableFolder, dependencySubFolder, dependencyArchitecture);
+
+			foreach (var sourceFile in Directory.GetFiles(dependencyFolder, "*", SearchOption.TopDirectoryOnly))
+			{
+				string fileName = Path.GetFileName(sourceFile);
+				string destinationFilePath = Path.Combine(executableFolder, fileName);
+				File.Copy(sourceFile, destinationFilePath, true);
+			}
+		}
+	}
+}
